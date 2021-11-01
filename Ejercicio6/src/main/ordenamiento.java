0c483ef3dcfb76f8d31a5736383b5cf7a8be8182
OscarFlorez <66635225+Santiago-Florez@users.noreply.github.com>
2021-11-01 00:02:10 -0500

    FALTA mejorar el caso 1 y el 2

--- Ejercicio6/src/main/ordenamiento.java
+++ Ejercicio6/src/main/ordenamiento.java
@@ -4,26 +4,25 @@ import java.util.Scanner;
 
 public class ordenamiento {
 
-	private static Scanner sc = new Scanner(System.in);;
+	private static Scanner sc = new Scanner(System.in);
+	private static ordenando order = new ordenando();;
 
 	public static void main(String[] args) {
-
-		if ("A".compareTo("B") < "B".compareTo("A")) {
-			System.out.println("Mi pana");
-		} else if ("B".compareTo("A") < "A".compareTo("B")) {
-			System.out.println("Hp");
-		}
-		System.out.println("Casos: ");
 		int numCasos = sc.nextInt();
 		int casos = 1;
 		while (numCasos > 0) {
-			System.out.println("Num Letras: ");
-			int numletras = sc.nextInt();
-			for (int i = 0; i < numletras; i++) {
-				System.out.println("Letras: ");
-				String letras = sc.next().toUpperCase();
-				System.out.println(letras);
+			sc.nextInt();
+			sc.nextLine();
+			String letras = sc.nextLine().toUpperCase();
+			String[] letrasI = letras.split(" ");
+			String operaciones = sc.nextLine().toUpperCase();
+			String[] conSeparadas = operaciones.split(" ");
+			for (int i = 0; i < conSeparadas.length; i++) {
+				order.orden(conSeparadas[i], casos);
 			}
+			System.out.println("Caso #"+casos);
+			System.out.println(order.resultado());
+			casos++;
 			numCasos--;
 		}
 
--- Ejercicio6/src/main/ordenando.java
+++ Ejercicio6/src/main/ordenando.java
@@ -3,8 +3,81 @@ package main;
 import java.util.ArrayList;
 
 public class ordenando {
-	
-	ArrayList<String> arSTR = new ArrayList<>();
-	
-	
+
+	private ArrayList<String> formaOrdenar;
+
+	public ordenando() {
+	}
+
+	public void orden(String condicion, int caso) {
+		formaOrdenar = new ArrayList<>();
+		ArrayList<String> arSTR = new ArrayList<>();
+		String[] letra_letra = condicion.split("");
+		for (int i = 0; i < letra_letra.length; i++) {
+			arSTR.add(letra_letra[i]);
+		}
+		if (formaOrdenar.toString().equals("[]")) {
+			if (arSTR.get(1).equals("<")) {
+				formaOrdenar.add(arSTR.get(0));
+				formaOrdenar.add(arSTR.get(2));
+			} else if (arSTR.get(1).equals(">")) {
+				formaOrdenar.add(arSTR.get(2));
+				formaOrdenar.add(arSTR.get(0));
+			}
+		} else {
+			if (arSTR.get(1).equals("<")) {
+				for (int i = 0; i < formaOrdenar.size(); i++) {
+					if (formaOrdenar.get(i).equals(arSTR.get(0))) {
+						formaOrdenar.add((i + 1), arSTR.get(2));
+					}
+				}
+			}
+			if (arSTR.get(1).equals(">")) {
+				for (int i = 0; i < formaOrdenar.size(); i++) {
+					if (formaOrdenar.get(i).equals(arSTR.get(0))) {
+						formaOrdenar.add(i, arSTR.get(2));
+						i++;
+					}
+				}
+			}
+
+			if (caso == 1) {
+				for (int i = 0; i < formaOrdenar.size(); i++) {
+					for (int j = 0; j < formaOrdenar.size(); j++) {
+						if (formaOrdenar.get(i).equals(formaOrdenar.get(j)) && i != j) {
+							formaOrdenar.remove(j);
+						}
+					}
+				}
+				formaOrdenar.add("G");
+			}
+			if (caso != 3 && caso != 1) {
+				for (int i = 0; i < formaOrdenar.size(); i++) {
+					for (int j = 0; j < formaOrdenar.size(); j++) {
+						if (formaOrdenar.get(i).equals(formaOrdenar.get(j)) && i != j) {
+							formaOrdenar.remove(j);
+						}
+					}
+				}
+			}
+		}
+		System.out.println(formaOrdenar.toString());
+	}
+
+	public String resultado() {
+		String ordenado = "";
+		for (String aux : formaOrdenar) {
+			ordenado += aux + " ";
+		}
+		return ordenado;
+	}
+
+	public ArrayList<String> getFormaOrdenar() {
+		return formaOrdenar;
+	}
+
+	public void setFormaOrdenar(ArrayList<String> formaOrdenar) {
+		this.formaOrdenar = formaOrdenar;
+	}
+
 }
